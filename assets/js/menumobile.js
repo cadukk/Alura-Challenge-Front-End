b6533b1fcf23337eab0553f6e3f32b36b9eec004
Cadu Ribeiro <57085486+cadukk@users.noreply.github.com>
2021-07-03 02:18:58 -0400

    implementacao barra busca mobile

--- assets/js/menumobile.js
+++ assets/js/menumobile.js
@@ -1,3 +1,5 @@
+// hamburger
+
 const $menu = document.querySelector('.barraMenuLateral')
 const $btnMenuOpen = document.querySelector('#hamburgerOpen')
 const $btnMenuClose = document.querySelector('#hamburgerClose')
@@ -12,4 +14,28 @@ $btnMenuClose.addEventListener('click', function () {
   $menu.classList.remove('abreMenuLateral')
   document.getElementById('hamburgerClose').style.display = "none"
   document.getElementById('hamburgerOpen').style.display = "flex"
+})
+
+// buscar
+
+const $menuBusca = document.querySelector('.menuBusca')
+const $btnBuscaOpen = document.querySelector('#buscaOpen')
+const $btnBuscaClose = document.querySelector('#buscaClose')
+
+$btnBuscaOpen.addEventListener('click', function () {
+  document.getElementById('buscaOpen').style.display = "none"
+  document.getElementById('buscaClose').style.display = "flex"
+  document.getElementById('buscaClose').style.marginRight = "155px"
+  document.getElementById('barraDeBusca').style.display = "block"
+
+  // document.getElementById('barraDeBusca').style.display = "flex"
+})
+$btnBuscaClose.addEventListener('click', function () {
+
+  document.getElementById('buscaClose').style.display = "none"
+  document.getElementById('buscaOpen').style.display = "flex"
+  document.getElementById('barraDeBusca').style.display = "none"
+
+})
+$btnBuscaOpen.addEventListener('click', function () {
 })
